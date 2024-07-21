2799424b7980b2a690a741863609d3bd038e9b7a
theonlyswissarmy <61249010+theonlyswissarmy@users.noreply.github.com>
2024-07-21 04:51:42 -0700

    Update GUI.py
    Ironing out kinks, Finishing main bot loop for GUI

--- GUI.py
+++ GUI.py
@@ -1,4 +1,43 @@
-import numpy as np, SAVEthetime as s, tkinter as tk
+import numpy as np, SAVEthetime as s, tkinter as tk, pyautogui as pya
+
+def helpstt():
+    s.openapopup("whadda hell")
+    print("do me soon")
+
+
+
+
+
+def doguibot(): # this is nearly a one to one copy of the main bot in SAVEthetime, but now its able to handle certain events
+    s.time.sleep(5)
+    s.grabEGO()
+    fail = True
+    while not s.keyboard.is_pressed('q'):
+        s.move()
+        print("EXITED MOVE")
+        eventck = s.getevent()
+        print("EXITED EVENT")
+        fightck = s.infightcheck()
+        egochk = s.grabEGO()
+        if not fightck:
+            if not eventck or not egochk:
+                if not s.isvictory():
+                    print("looking around and adjusting zoom")
+                    fail = s.adjustzoom()
+                    if fail or not egochk:
+                        print("Could not find icon, exiting")
+                        exit(0)
+                else:
+                    exit(0)  # We won!
+            else:
+                print("getting zoom right after event")
+                s.pyautogui.scroll(-1)
+                s.pyautogui.scroll(1)
+        else:  # we're in a fight
+            s.mainfight()
+
+def doluxnow():
+    s.dolux()
 sinnames = ["1. Yi Sang:", "2. Faust:", "3. Don Quixote:",
             "4. Ryoshu:", "5. Meursault:", "6. Hong Lu:", "7.Heathcliffe:",
             "8. Ishmael:","9. Rodion:","10. Sinclair:","11. Outis:","12. Gregor:"]
@@ -21,16 +60,11 @@ for i in range(4):
         entries[sin].grid(row=j+2, column= (i*2)+1)
         sin = sin+1
 
-button[0] = tk.Button(window, text="Help", command=lambda: domainbot())
-button[1] = tk.Button(window, text="Start Bot", command=lambda: domainbot())
-button[2] = tk.Button(window, text="Do Lux", command=lambda: domainbot())
-s.mainbot()
+button[0] = tk.Button(window, text="Help", command=lambda: helpstt())
+button[1] = tk.Button(window, text="Start Bot", command=lambda: doguibot())
+button[2] = tk.Button(window, text="Do Lux", command=lambda: doluxnow())
 checkme.grid(row=5, column=2,columnspan=3)
-
+button[0].grid(row=6, column=2)
+button[1].grid(row=6, column=3)
+button[2].grid(row=6, column=4)
 window.mainloop()
-
-def doguibot():
-    s.mainbot()
-
-def doluxnow():
-    s.dolux()
